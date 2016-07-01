1eefdd4d129aeaf88a67fbb9b78f57b0b6eb3d1d
YC <YC@users.noreply.github.com>
2016-07-01 17:05:05 +1000

    Code cleanup Improve home page loading Reactivate syntax highlighting and change theme to base16 Tomorrow Update third-party JS files

--- js/script.js
+++ js/script.js
@@ -1,15 +1,8 @@
-/*global smooth_scroll_to, YT*/
+/* global smooth_scroll_to, YT, document, Image */
 
-// Return Location of Tag
-function returnlocation(pageid) {
-    "use strict";
-    var location = (document.getElementById(pageid).getBoundingClientRect().top) - document.body.getBoundingClientRect().top;
-    return location;
-}
-
+// Load Youtube Player
+// https://developers.google.com/youtube/iframe_api_reference#Loading_a_Video_Player
 var player;
-
-
 function loadYT() {
     "use strict";
     document.getElementById('video').className = "animateinvideo";
@@ -18,14 +11,11 @@ function loadYT() {
     tag.src = "https://www.youtube.com/iframe_api";
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 }
-
-
 function onPlayerReady(event) {
     "use strict";
     event.target.setVolume(100);
     event.target.playVideo();
 }
-
 function onPlayerStateChange(event) {
     "use strict";
     if (event.data === 0) {
@@ -33,8 +23,6 @@ function onPlayerStateChange(event) {
         document.getElementById('menu').className = "animatemenu";
     }
 }
-
-//https://developers.google.com/youtube/iframe_api_reference#Loading_a_Video_Player
 function onYouTubeIframeAPIReady() {
     "use strict";
     player = new YT.Player('player', {
@@ -46,57 +34,47 @@ function onYouTubeIframeAPIReady() {
 }
 
 
+// Return Location of Tag
+function returnlocation(pageid) {
+    "use strict";
+    var location = (document.getElementById(pageid).getBoundingClientRect().top) - document.body.getBoundingClientRect().top;
+    return location;
+}
+// Navigation Bar Clicks
 function initlinks() {
     "use strict";
+    var time = 1000;
     document.getElementById('herolink').onclick = function () {
-        smooth_scroll_to(returnlocation('hero'), 1000);
+        smooth_scroll_to(returnlocation('hero'), time);
     };
     document.getElementById('portfoliolink').onclick = function () {
-        smooth_scroll_to(returnlocation('portfolio'), 1000);
+        smooth_scroll_to(returnlocation('portfolio'), time);
     };
     document.getElementById('arrowcontain').onclick = function () {
-        smooth_scroll_to(returnlocation('portfolio'), 1000);
+        smooth_scroll_to(returnlocation('portfolio'), time);
     };
     document.getElementById('videolink').onclick = function () {
-        smooth_scroll_to(returnlocation('video'), 1000);
+        smooth_scroll_to(returnlocation('video'), time);
         loadYT();
     };
 }
 
-
-//function menuanimation() {
-//    "use strict";
-//    if (window.scrollY >= document.getElementById('video').offsetTop && window.scrollY < document.getElementById('portfolio').offsetTop) {
-//        document.getElementById('menu').className = "animateoutmenu";
-//    } else {
-//        document.getElementById('menu').className = "";
-//    }
-//}
-
+// Fadein Element
 function fadein(e) {
     "use strict";
-    e.className = e.className + " fadein";
+    e.className = "fadein";
+    e.style.backgroundImage = "img/hero.png";
 }
-
 // Document Load
 document.onreadystatechange = function () {
     "use strict";
-    if (document.readyState === "complete") {
-
+    if (document.readyState === "interactive") {
         initlinks();
-        //menuanimation();
-        var project = document.getElementById('hero');
-        project.onload = fadein(project);
-
-        //smooth_scroll_to(returnlocation('hero'), 1000);
+        var image = new Image();
+        image.src = "img/hero.png";
+        image.onload = function() {
+            var project = document.getElementById('hero');
+            project.onload = fadein(project);
+        };
     }
 };
-
-//document.onscroll = function () {
-//    "use strict";
-//    //menuanimation();
-//    //console.log("Main " + document.getElementById('hero').offsetTop);
-//    //console.log("Player " + document.getElementById('player').offsetTop);
-//    //console.log("Portfolio " + document.getElementById('portfolio').offsetTop);
-//    //console.log(window.scrollY);
-//};
--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,9 +1,12 @@
-/* Author/Source: https://coderwall.com/p/hujlhg/smooth-scrolling-without-jquery*/
-/* https://developer.mozilla.org/en-US/docs/Web/API/Window.scrollY*/
-/* CFWS: Modified as it doesn't work with Firefox to scroll the entire page (document.body) - 
-Changed to window.scrollY and window.scroll(0, target) -> Where 0 is for Horizontal*/
+/* global window, document, Promise, navigator */
+/* Author/Source: https://coderwall.com/p/hujlhg/smooth-scrolling-without-jquery */
+/* https://developer.mozilla.org/en-US/docs/Web/API/Window.scrollY */
+/*
+    CFWS: Modified as it doesn't work with Firefox to scroll the entire page (document.body)
+    Changed to window.scrollY and window.scroll(0, target) -> Where 0 is for Horizontal
+ */
 
-/**
+/*
     Smoothly scroll element to the given target (element.scrollTop)
     for the given duration
 
@@ -33,10 +36,7 @@ var smooth_scroll_to = function (target, duration) {
 
     var start_time = Date.now();
     var end_time = start_time + duration;
-
-    //var start_top = element.scrollTop;
     var start_top = scrollPosition();
-
     var distance = target - start_top;
 
     // based on http://en.wikipedia.org/wiki/Smoothstep
@@ -60,21 +60,17 @@ var smooth_scroll_to = function (target, duration) {
 
         // This is like a think function from a game loop
         var scroll_frame = function () {
-            //if(element.scrollTop != previous_top) {
             if (scrollPosition() !== previous_top) {
-                if (navigator.userAgent.match(/iPhone/i) === false || (navigator.userAgent.match(/iPod/i))) === false {
-                                reject("interrupted");
-                return;
-
+                if (navigator.userAgent.match(/iPhone/i) === false || (navigator.userAgent.match(/iPod/i)) === false) {
+                    reject("interrupted");
+                    return;
                 }
-                
             }
 
             // set the scrollTop for this frame
             var now = Date.now();
             var point = smooth_step(start_time, end_time, now);
             var frameTop = Math.round(start_top + (distance * point));
-            //element.scrollTop = frameTop;
             window.scroll(0, frameTop);
 
             // check if we're done!
@@ -82,22 +78,6 @@ var smooth_scroll_to = function (target, duration) {
                 resolve();
                 return;
             }
-
-            // If we were supposed to scroll but didn't, then we
-            // probably hit the limit, so consider it done; not
-            // interrupted.
-            /*            if(element.scrollTop === previous_top
-                && element.scrollTop !== frameTop) {
-                resolve();
-                return;
-            }
-
-            if(scrollPosition() === previous_top
-               && scrollPosition() !== frameTop) {
-                resolve();
-                return;
-            }
-            */
 
             if (target === scrollPosition) {
                 resolve();
@@ -107,10 +87,10 @@ var smooth_scroll_to = function (target, duration) {
             previous_top = scrollPosition();
 
             // schedule next frame for execution
-            setTimeout(scroll_frame, 0);
+            window.setTimeout(scroll_frame, 0);
         };
 
         // boostrap the animation process
-        setTimeout(scroll_frame, 0);
+        window.setTimeout(scroll_frame, 0);
     });
 };
